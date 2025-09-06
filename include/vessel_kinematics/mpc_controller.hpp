cb1555a80410d070d2e3b995eb5024875e8b5bc4
imirfurkan <furkan4ness@gmail.com>
2025-09-06 21:18:42 +0300

    set up the solver

--- include/vessel_kinematics/mpc_controller.hpp
+++ include/vessel_kinematics/mpc_controller.hpp
@@ -8,12 +8,12 @@ using namespace casadi;
 // --- MPC Tuning Parameters ---
 struct MPCParams
 {
-  unsigned int    P_;       // Prediction horizon (number of steps)
-  double          Ts_;      // Control timestep (seconds), e.g., 0.1s for 10Hz
-  casadi::DM      Q_;       // Weighting matrix for state error (augmented)
-  casadi::DM      R_;       // Weighting matrix for control input changes
-  Eigen::Vector3d tau_min_; // Minimum thruster force/torque
-  Eigen::Vector3d tau_max_; // Maximum thruster force/torque
+  int        P_;       // Prediction horizon (number of steps)
+  double     Ts_;      // Control timestep (seconds), e.g., 0.1s for 10Hz
+  casadi::DM Q_;       // Weighting matrix for state error (augmented)
+  casadi::DM R_;       // Weighting matrix for control input changes
+  casadi::DM tau_min_; // Minimum thruster force/torque
+  casadi::DM tau_max_; // Maximum thruster force/torque
 };
 
 struct MPCState
@@ -22,18 +22,12 @@ struct MPCState
   Eigen::Vector<double, 6> x_k_;           // Current full state [nu, eta]
   Eigen::Vector<double, 6> x_k_minus_1_;   // Previous full state (for delta_x)
   Eigen::Vector3d          tau_k_minus_1_; // Last applied control input (for delta_tau)
-
-  // The numerical matrices for the augmented model p[k+1] = A_bar*p[k] + B_bar*delta_tau[k]
-  // These will be recalculated in every loop.
-  Eigen::Matrix<double, 9, 9> A_bar_;
-  Eigen::Matrix<double, 9, 3> B_bar_;
 };
 
-struct solverObjects
+struct MPCSolver
 {
   // --- CasADi Solver Objects ---
-  std::shared_ptr<casadi::Opti> opti_;   // The optimization problem stack
-  casadi::Function              solver_; // The compiled solver function
+  std::shared_ptr<casadi::Opti> opti_; // The optimization problem stack
 
   // Symbolic VARIABLES the solver will find
   casadi::MX U_var_; // The sequence of control changes {Δτ_k, ..., Δτ_{k+P-1}}
@@ -41,6 +35,11 @@ struct solverObjects
 
   // Symbolic PARAMETERS we will provide at runtime
   casadi::MX p0_param_;       // The initial augmented state p[k]
-  casadi::MX pref_param_;     // The reference for the output y (part of p)
+  casadi::MX p_ref_param_;    // The reference for the output y (part of p)
   casadi::MX tau_prev_param_; // The previous control input τ[k-1]
+
+  // Matrices for the augmented model p[k+1] = A_bar*p[k] + B_bar*delta_tau[k]
+  // These will be recalculated in every loop.
+  casadi::MX A_bar_;
+  casadi::MX B_bar_;
 };
--- src/mpc_controller_node.cpp
+++ src/mpc_controller_node.cpp
@@ -20,7 +20,13 @@ class MPCNode : public rclcpp::Node
 public:
   MPCNode() : Node("mpc_controller_node")
   {
+    //----------------------------------------------------------------
+    // Variable initialization
+    //----------------------------------------------------------------
     this->loadParameters();
+    mpc_state_.x_k_.setZero();
+    mpc_state_.x_k_minus_1_.setZero();
+    mpc_state_.tau_k_minus_1_.setZero();
     //----------------------------------------------------------------
     // ROS interfaces
     //----------------------------------------------------------------
@@ -34,6 +40,9 @@ public:
 
     // calls step() each 20ms
     timer_ = create_wall_timer(20ms, std::bind(&MPCNode::controllerLoop, this));
+
+    // --
+    this->setupSolver();
   }
 
 private:
@@ -72,28 +81,79 @@ private:
     eta_(2) = vk::wrapAngle(M_PI / 2.0 - yaw_enu); //    // convert ENU yaw (CCW, 0 = East) to NED yaw (CW, 0 = North)
   }
   void controllerLoop() {}
-  void setupSolver() {}
+  void setupSolver()
+  {
+    mpc_solver_.opti_ = std::make_shared<casadi::Opti>();
+    const int p_dim   = 9; // Dimension of augmented state p = [Δx, y]
+    const int u_dim   = 3; // Dimension of control input u = Δτ
+
+    // Variables the solver will find
+    mpc_solver_.U_var_ = mpc_solver_.opti_->variable(u_dim, mpc_params_.P_);
+    mpc_solver_.P_var_ = mpc_solver_.opti_->variable(p_dim, mpc_params_.P_ + 1);
+
+    // Parameters we will provide at runtime
+    mpc_solver_.p0_param_       = mpc_solver_.opti_->parameter(p_dim, 1);
+    mpc_solver_.p_ref_param_    = mpc_solver_.opti_->parameter(p_dim, 1);
+    mpc_solver_.tau_prev_param_ = mpc_solver_.opti_->parameter(u_dim, 1);
+    mpc_solver_.A_bar_          = mpc_solver_.opti_->parameter(p_dim, p_dim);
+    mpc_solver_.B_bar_          = mpc_solver_.opti_->parameter(p_dim, u_dim);
+
+    // --- Symbolic Cost Function ---
+    casadi::MX cost = 0;
+    for (int i = 0; i < mpc_params_.P_; i++)
+    {
+      auto state_error    = mpc_solver_.P_var_(Slice(), i + 1) - mpc_solver_.p_ref_param_;
+      auto control_change = mpc_solver_.U_var_(Slice(), i);
+      cost += casadi::MX::mtimes({state_error.T(), mpc_params_.Q_, state_error});
+      cost += casadi::MX::mtimes({control_change.T(), mpc_params_.R_, control_change});
+    }
+    mpc_solver_.opti_->minimize(cost);
+
+    // --- Define Prediction Constraints ---
+    // Initial state constraint
+    mpc_solver_.opti_->subject_to(mpc_solver_.P_var_(Slice(), 0) == mpc_solver_.p0_param_);
+    // Dynamic constraints over the horizon
+    // p[k+i+1] = Ā * p[k+i] + B̄ * Δτ[k+i]
+    for (int i = 0; i < mpc_params_.P_; i++)
+    {
+      mpc_solver_.opti_->subject_to(mpc_solver_.P_var_(Slice(), i + 1) ==
+                                    casadi::MX::mtimes(mpc_solver_.A_bar_, mpc_solver_.P_var_(Slice(), i)) +
+                                        casadi::MX::mtimes(mpc_solver_.B_bar_, mpc_solver_.U_var_(Slice(), i)));
+    }
+    // --- Define Control Constraints ---
+    casadi::MX tau_i = mpc_solver_.tau_prev_param_; // last known thrust
+    for (int i = 0; i < mpc_params_.P_; i++)
+    {
+      tau_i += mpc_solver_.U_var_(Slice(), i);
+
+      mpc_solver_.opti_->subject_to(tau_i >= mpc_params_.tau_min_);
+      mpc_solver_.opti_->subject_to(tau_i <= mpc_params_.tau_max_);
+    }
+    // --- Create the Solver ---
+    casadi::Dict opts;
+    opts["qpsol"]         = "qpoases";
+    opts["qpsol_options"] = casadi::Dict{{"printLevel", "none"}};
+    opts["print_time"]    = false;
+    opts["expand"]        = true;
+
+    mpc_solver_.opti_->solver("sqpmethod", opts);
+    RCLCPP_INFO(this->get_logger(), "MPC solver has been set up successfully.");
+  }
   void loadParameters()
   {
     MPCParams p_default;
 
-    mpc_params_.P_  = this->declare_parameter<unsigned int>("P", p_default.P_);
+    mpc_params_.P_  = this->declare_parameter<int>("P", p_default.P_);
     mpc_params_.Ts_ = this->declare_parameter<double>("Ts", p_default.Ts_);
 
-    auto tau_min_vec = this->declare_parameter<std::vector<double>>(
-        "tau_min", {p_default.tau_min_(0), p_default.tau_min_(1), p_default.tau_min_(2)});
-    mpc_params_.tau_min_ = Eigen::Map<Eigen::Vector3d>(tau_min_vec.data());
-
-    auto tau_max_vec = this->declare_parameter<std::vector<double>>(
-        "tau_max", {p_default.tau_max_(0), p_default.tau_max_(1), p_default.tau_max_(2)});
-    mpc_params_.tau_max_ = Eigen::Map<Eigen::Vector3d>(tau_max_vec.data());
-
-    // Q and R are guaranteed to be in YAML
-    std::vector<double> Q_vec = this->get_parameter("Q_weights").as_double_array();
-    std::vector<double> R_vec = this->get_parameter("R_weights").as_double_array();
-
-    mpc_params_.Q_ = casadi::DM::reshape(Q_vec, 3, 3);
-    mpc_params_.R_ = casadi::DM::reshape(R_vec, 3, 3);
+    std::vector<double> Q_vec       = this->get_parameter("Q_weights").as_double_array();
+    std::vector<double> R_vec       = this->get_parameter("R_weights").as_double_array();
+    std::vector<double> tau_min_vec = this->get_parameter("tau_min").as_double_array();
+    std::vector<double> tau_max_vec = this->get_parameter("tau_max").as_double_array();
+    mpc_params_.Q_                  = casadi::DM::reshape(Q_vec, 3, 3);
+    mpc_params_.R_                  = casadi::DM::reshape(R_vec, 3, 3);
+    mpc_params_.tau_min_            = casadi::DM::reshape(tau_min_vec, 3, 1);
+    mpc_params_.tau_max_            = casadi::DM::reshape(tau_max_vec, 3, 1);
   }
   // ROS2 Subscribers and Publishers
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pos_cmd_sub_;
@@ -106,6 +166,8 @@ private:
   Eigen::Vector3d eta_des_; // [N, E, ψ]
   Eigen::Vector3d nu_;      // body-frame twist [u, v, r]
   MPCParams       mpc_params_;
+  MPCState        mpc_state_;
+  MPCSolver       mpc_solver_;
 
   bool goal_received_ = false;
 };
